e3b592aa96c4d72a8be20b5ea8f7dc8d9c64d0d1
Matěj Šmíd <smidm@cmp.felk.cvut.cz>
2016-12-21 15:12:27 +0100

    changed maturity to alpha

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ except ImportError:
 
 setup(
     name='flashvideosynchronization',
-    version='1.0b1',
+    version='1.0a1',
     author='Matěj Šmíd',
     url='https://github.com/smidm/flashvideosynchronization',
     author_email='m@matejsmid.cz',
     license='The MIT License',
     long_description='Sub-millisecond accurate multiple video synchronization using camera flashes.',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
 
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Image Recognition',
