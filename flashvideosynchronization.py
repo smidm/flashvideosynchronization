7594884c3190b2103c3f39af48f07144ef94dc06
Matěj Šmíd <m@matejsmid.cz>
2020-11-08 23:17:06 +0100

    port sample code to py3 and change sample data

--- flashvideosynchronization.py
+++ flashvideosynchronization.py
@@ -633,16 +633,15 @@ class FlashVideoSynchronization(object):
         self.base_cam = data['base_cam']
 
 
-
 if __name__ == '__main__':
-    # example 4 camera synchronization
-    cameras = [1, 2, 3, 4]
-    filenames = {cam: 'data/ice_hockey/%d.mp4' % cam for cam in cameras}
+    # example 3 camera synchronization
+    cameras = [1, 2, 3]
+    filenames = {cam: 'sample_data/%d.mp4' % cam for cam in cameras}
 
     # load video files and extract frame timestamps
     sources = {cam: imagesource.TimedVideoSource(filenames[cam])
                for cam in cameras}
-    for source in sources.itervalues():
+    for source in sources.values():
         source.extract_timestamps()
 
     # detect flash events
@@ -655,8 +654,8 @@ if __name__ == '__main__':
 
     # manually set rough offset by matching an event
     sync.show_events()
-    matching_events = {1: 3, 3: 2, 2: 8, 4: 2}
-    offsets = {cam: sync.events[cam][matching_events[cam]]['time'] for cam in cameras}
+    matching_events = {1: 0, 2: 0, 3: 0}
+    offsets = {cam: sync.events[cam][matching_events[cam]]['frame_time'] for cam in cameras}
     sync.show_events(offsets)  # now the events should appear aligned
 
     # # optionally filter bad events and fix some wrongly detected
@@ -673,16 +672,16 @@ if __name__ == '__main__':
 
     # synchronize cameras: find parameters transformations that map camera time to reference camera time
     sync.synchronize(cameras, offsets, base_cam=1)
-    print sync.model
+    print(sync.model)
 
-    print sync.get_time(1, 0, 1000)
+    print(sync.get_time(1, 0, 1000))
 
     # get frame synchronized image sources
     sources_sync = sync.get_synchronized_image_sources(sources, master=1, dropped=False)  # , perfect_master=False)
 
     # use the synchronized video sources to show synchronized frames with time deviations
     frame = 0
-    for i, (cam, source) in enumerate(sources_sync.iteritems()):
+    for i, (cam, source) in enumerate(sources_sync.items()):
         plt.figure()
         plt.title('err: %02.f ms' % source.get_synchronization_error(frame))
         img = source.get_image(frame)
